b34a889c64645e37a9b0614fff347b20a1dca864
Radio <152258403+Elesovitost@users.noreply.github.com>
2025-06-10 13:43:56 +0200

    Add files via upload

--- WORDS/nazvy.ps1
+++ WORDS/nazvy.ps1
@@ -1,4 +1,3 @@
 $files = Get-ChildItem -File | Where-Object { $_.Extension -match '^\.(jpg|jpeg)$' }
-$names = $files | Select-Object -ExpandProperty BaseName | ForEach-Object { "'$_'" }
-$jsArray = "const words = [" + ($names -join ", ") + "];"
-Set-Content -Path "nazvy.txt" -Value $jsArray
+$names = $files | Select-Object -ExpandProperty BaseName
+Set-Content -Path "words.txt" -Value $names -Encoding UTF8
